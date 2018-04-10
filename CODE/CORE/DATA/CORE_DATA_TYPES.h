c8c5ddbc3e88401a565aa9b36cda0e35a1f48ca1
Christophe Bernard <x_tox_man@hotmail.com>
2018-04-10 19:34:03 +0200

    missing stuff

--- CODE/CORE/DATA/CORE_DATA_TYPES.h
+++ CODE/CORE/DATA/CORE_DATA_TYPES.h
@@ -23,7 +23,7 @@ typedef wchar_t ** WCHAR_ARRAY_POINTER_POINTER;
 #define CORE_DATA_COPY_STRING_WITH_SIZE(__DESTINATION__, __SOURCE__, __SIZE__) \
     { strcpy_s(__DESTINATION__, __SIZE__, __SOURCE__); }
 #define CORE_DATA_STRING_CONCAT( __DESTINATION__, __SOURCE__) \
-    { size_t ___s = (strlen(__SOURCE__) ); if ( ___s++ ) strcat_s(__DESTINATION__, ___s, __SOURCE__); }
+    { size_t ___s = (strlen(__SOURCE__) ) + (strlen(__DESTINATION__) ); if ( ___s++ ) strcat_s(__DESTINATION__, ___s, __SOURCE__); }
 #define CORE_DATA_COPY_WIDE_STRING(__DESTINATION__, __SOURCE__) \
     { size_t ___s = (wcslen(__SOURCE__) ); if ( ___s++ ) wcscpy_s(__DESTINATION__, ___s, __SOURCE__); }
 
--- CODE/CORE/DATA/STREAM/CORE_DATA_STREAM.h
+++ CODE/CORE/DATA/STREAM/CORE_DATA_STREAM.h
@@ -16,267 +16,279 @@
 
 XS_CLASS_BEGIN( CORE_DATA_STREAM )
 
-    CORE_DATA_STREAM();
-    CORE_DATA_STREAM( int size );
-    CORE_DATA_STREAM( const char * pointer, int size );
-    ~CORE_DATA_STREAM();
+CORE_DATA_STREAM();
+CORE_DATA_STREAM( int size );
+CORE_DATA_STREAM( const char * pointer, int size );
+~CORE_DATA_STREAM();
 
-    void Open();
-    void Close();
-    void Reset();
+void Open();
+void Close();
+void Reset();
 
-    void InitializeWithSize( int size );
+void InitializeWithSize( int size );
 
-    void ResetOffset();
-    inline void SetOffset( int offset ) { Offset = offset; }
+void ResetOffset();
+inline void SetOffset( int offset ) {
+    Offset=offset;
+}
 
-    inline size_t GetSize() const { return Size; }
-    inline size_t GetAllocatedBytes() const { return AllocatedBytes; }
+inline size_t GetSize() const {
+    return Size;
+}
+inline size_t GetAllocatedBytes() const {
+    return AllocatedBytes;
+}
 
-    template <typename __TYPE__ >
-    void operator << ( const __TYPE__ & data ) {
-        
-        if ( AllocatedBytes - Offset < sizeof( __TYPE__ ) ) {
-            
-            AllocatedBytes += AllocatedBytes + sizeof( __TYPE__ );
-            
-            void * new_buffer = realloc( MemoryBuffer, AllocatedBytes );
-            
-            #if DEBUG
-                if ( new_buffer == NULL ) {
-                
-                    CORE_RUNTIME_Abort();
-                }
-            #endif
-            
-            MemoryBuffer = new_buffer;
-        }
-        
-        //TODO : byteswap :
-        
-        CORE_MEMORY_COPY_BYTES_SWAPPED< __TYPE__ >( MemoryBuffer, Offset, data );
-        
-        Offset += sizeof( __TYPE__ );
-    }
+template <typename __TYPE__ >
+void operator << ( const __TYPE__ & data ) {
 
-    template <typename __TYPE__ >
-    void operator << ( const __TYPE__ * data ) {
-        
-        if ( AllocatedBytes - Offset < sizeof( __TYPE__ ) ) {
-            
-            AllocatedBytes += AllocatedBytes + sizeof( __TYPE__ );
-            
-            MemoryBuffer = realloc( MemoryBuffer, AllocatedBytes );
-            
-            #if DEBUG
-                if ( MemoryBuffer == NULL ) {
-                    
-                    CORE_RUNTIME_Abort();
-                }
-            #endif
-        }
-        
-        CORE_RUNTIME_Abort();
-    }
+    if ( AllocatedBytes - Offset < sizeof( __TYPE__ ) ) {
 
-    template <typename __TYPE__ >
-    void operator >> ( __TYPE__ & data ) {
-        
-        memcpy( &data, ( char* )MemoryBuffer + Offset, sizeof(__TYPE__ ) );
-        
-        Offset += sizeof( __TYPE__ );
-    }
+        AllocatedBytes+=AllocatedBytes + sizeof( __TYPE__ );
 
-    void operator >> ( wchar_t ** data ) {
-        
-        size_t
-            length;
-        
-        memcpy( &length, (( char* )MemoryBuffer + Offset), sizeof(size_t) );
-        Offset  += sizeof(size_t);
-        
-        *data = (wchar_t *) CORE_MEMORY_ALLOCATOR::Allocate( length );
-        
-        memcpy( data, ((( char* )MemoryBuffer) + Offset), length );
-        
-        Offset += length;
-    }
+        void * new_buffer=realloc( MemoryBuffer, AllocatedBytes );
 
-    inline const void * GetMemoryBuffer() const { return MemoryBuffer; }
-    inline void * GetMemoryBuffer() { return MemoryBuffer; }
-
-    inline size_t GetOffset() { return Offset; }
-
-    void InputBytesWithoutSize( const char * pointer, int size ) {
-        
-        size_t
-            length = size * sizeof( char );
-        
-        if ( AllocatedBytes - Offset < (size_t) size ) {
-            
-            AllocatedBytes += AllocatedBytes + size;
-            
-            void * new_buffer = realloc( MemoryBuffer, AllocatedBytes );
-            
 #if DEBUG
-            if ( new_buffer == NULL ) {
-                
-                CORE_RUNTIME_Abort();
-            }
+        if ( new_buffer == NULL ) {
+
+            CORE_RUNTIME_Abort();
+        }
 #endif
-            
-            MemoryBuffer = new_buffer;
+
+        MemoryBuffer=new_buffer;
+    }
+
+    //TODO : byteswap :
+
+    CORE_MEMORY_COPY_BYTES_SWAPPED< __TYPE__ >( MemoryBuffer, Offset, data );
+
+    Offset+=sizeof( __TYPE__ );
+}
+
+template <typename __TYPE__ >
+void operator << ( const __TYPE__ * data ) {
+
+    if ( AllocatedBytes - Offset < sizeof( __TYPE__ ) ) {
+
+        AllocatedBytes+=AllocatedBytes + sizeof( __TYPE__ );
+
+        MemoryBuffer=realloc( MemoryBuffer, AllocatedBytes );
+
+#if DEBUG
+        if ( MemoryBuffer == NULL ) {
+
+            CORE_RUNTIME_Abort();
         }
-        
-        memcpy( ((( char* )MemoryBuffer) + Offset), pointer, length );
-        Offset += length;
+#endif
     }
 
-    template < typename __TYPE__ >
-    void InputBytes(__TYPE__ ** pointer, int size ) {
-        
-        size_t
-            length = size * sizeof( __TYPE__ );
-        
-        if ( AllocatedBytes - Offset < length + sizeof(size_t) ) {
-            
-            AllocatedBytes += AllocatedBytes + length + sizeof(size_t);
-            
-            MemoryBuffer = realloc( MemoryBuffer, AllocatedBytes );
-            
-    #if DEBUG
-            if ( MemoryBuffer == NULL ) {
-                
-                CORE_RUNTIME_Abort();
-            }
-    #endif
+    CORE_RUNTIME_Abort();
+}
+
+template <typename __TYPE__ >
+void operator >> ( __TYPE__ & data ) {
+
+    memcpy( &data, ( char* ) MemoryBuffer + Offset, sizeof( __TYPE__ ) );
+
+    Offset+=sizeof( __TYPE__ );
+}
+
+void operator >> ( wchar_t ** data ) {
+
+    size_t
+        length;
+
+    memcpy( &length, ( ( char* ) MemoryBuffer + Offset ), sizeof( size_t ) );
+    Offset+=sizeof( size_t );
+
+    *data=( wchar_t * ) CORE_MEMORY_ALLOCATOR::Allocate( length );
+
+    memcpy( data, ( ( ( char* ) MemoryBuffer ) + Offset ), length );
+
+    Offset+=length;
+}
+
+inline const void * GetMemoryBuffer() const {
+    return MemoryBuffer;
+}
+inline void * GetMemoryBuffer() {
+    return MemoryBuffer;
+}
+
+inline size_t GetOffset() {
+    return Offset;
+}
+
+void InputBytesWithoutSize( const char * pointer, int size ) {
+
+    size_t
+        length=size * sizeof( char );
+
+    if ( AllocatedBytes - Offset < ( size_t ) size ) {
+
+        AllocatedBytes+=AllocatedBytes + size;
+
+        void * new_buffer=realloc( MemoryBuffer, AllocatedBytes );
+
+#if DEBUG
+        if ( new_buffer == NULL ) {
+
+            CORE_RUNTIME_Abort();
         }
-        
-        memcpy( ((( char* )MemoryBuffer) + Offset), &length, sizeof(size_t) );
-        Offset  += sizeof(size_t);
-        memcpy( ((( char* )MemoryBuffer) + Offset), *pointer, length );
-        Offset += length;
+#endif
+
+        MemoryBuffer=new_buffer;
     }
 
-    template < typename __TYPE__ >
-    void OutputBytes(__TYPE__ ** pointer, int & size ) {
-        
-        size_t
-            length;
-        
-        memcpy( &length, (( char* )MemoryBuffer + Offset), sizeof(size_t) );
-        Offset  += sizeof(size_t);
-        
-        *pointer = ( __TYPE__ *) CORE_MEMORY_ALLOCATOR::Allocate( length * sizeof( __TYPE__ ) );
-        
-        memcpy( *pointer, ((( char* )MemoryBuffer) + Offset), length );
-        
-        Offset  += length;
-        size = length;
-    }
+    memcpy( ( ( ( char* ) MemoryBuffer ) + Offset ), pointer, length );
+    Offset+=length;
+}
 
-    //Stupid fix with size_t
-    template < typename __TYPE__ >
-    void InputBytes(__TYPE__ * pointer, size_t size ) {
-        
-        unsigned long length = size * sizeof( __TYPE__ );
-        
-        if ( AllocatedBytes - Offset < length + sizeof(size_t) ) {
-            
-            AllocatedBytes += AllocatedBytes + length + sizeof(size_t);
-            
-            MemoryBuffer = realloc( MemoryBuffer, AllocatedBytes );
-            
-    #if DEBUG
-            if ( MemoryBuffer == NULL ) {
-                
-                CORE_RUNTIME_Abort();
-            }
-    #endif
+template < typename __TYPE__ >
+void InputBytes( __TYPE__ ** pointer, int size ) {
+
+    size_t
+        length=size * sizeof( __TYPE__ );
+
+    if ( AllocatedBytes - Offset < length + sizeof( size_t ) ) {
+
+        AllocatedBytes+=AllocatedBytes + length + sizeof( size_t );
+
+        MemoryBuffer=realloc( MemoryBuffer, AllocatedBytes );
+
+#if DEBUG
+        if ( MemoryBuffer == NULL ) {
+
+            CORE_RUNTIME_Abort();
         }
-        
-        memcpy( ((( char* )MemoryBuffer) + Offset), &length, sizeof(size_t) );
-        Offset  += sizeof(size_t);
-        memcpy( ((( char* )MemoryBuffer) + Offset), (void*)pointer, length );
-        Offset += length;
+#endif
     }
 
-    template < typename __TYPE__ >
-    void InputBytes(__TYPE__ * & pointer, int size ) {
-        
-        size_t
-            length = size * sizeof( __TYPE__ );
-        
-        if ( AllocatedBytes - Offset < length + sizeof(size_t) ) {
-            
-            AllocatedBytes += AllocatedBytes + length + sizeof(size_t);
-            
-            MemoryBuffer = realloc( MemoryBuffer, AllocatedBytes );
-            
-    #if DEBUG
-            if ( MemoryBuffer == NULL ) {
-                
-                CORE_RUNTIME_Abort();
-            }
-    #endif
+    memcpy( ( ( ( char* ) MemoryBuffer ) + Offset ), &length, sizeof( size_t ) );
+    Offset+=sizeof( size_t );
+    memcpy( ( ( ( char* ) MemoryBuffer ) + Offset ), *pointer, length );
+    Offset+=length;
+}
+
+template < typename __TYPE__ >
+void OutputBytes( __TYPE__ ** pointer, int & size ) {
+
+    size_t
+        length;
+
+    memcpy( &length, ( ( char* ) MemoryBuffer + Offset ), sizeof( size_t ) );
+    Offset+=sizeof( size_t );
+
+    *pointer=( __TYPE__ * ) CORE_MEMORY_ALLOCATOR::Allocate( length * sizeof( __TYPE__ ) );
+
+    memcpy( *pointer, ( ( ( char* ) MemoryBuffer ) + Offset ), length );
+
+    Offset+=length;
+    size=length;
+}
+
+//Stupid fix with size_t
+template < typename __TYPE__ >
+void InputBytes( __TYPE__ * pointer, size_t size ) {
+
+    unsigned long length=size * sizeof( __TYPE__ );
+
+    if ( AllocatedBytes - Offset < length + sizeof( size_t ) ) {
+
+        AllocatedBytes+=AllocatedBytes + length + sizeof( size_t );
+
+        MemoryBuffer=realloc( MemoryBuffer, AllocatedBytes );
+
+#if DEBUG
+        if ( MemoryBuffer == NULL ) {
+
+            CORE_RUNTIME_Abort();
         }
-        
-        memcpy( ((( char* )MemoryBuffer) + Offset), &length, sizeof(size_t) );
-        Offset  += sizeof(size_t);
-        memcpy( ((( char* )MemoryBuffer) + Offset), (void*)pointer, length );
-        Offset += length;
+#endif
     }
 
-    template < typename __TYPE__ >
-    void OutputBytes(__TYPE__ * & pointer, int & size ) {
-        
-        size_t
-            length;
-        
-        memcpy( &length, (( char* )MemoryBuffer + Offset), sizeof(size_t) );
-        Offset  += sizeof(size_t);
-        
-        pointer = ( __TYPE__ *) CORE_MEMORY_ALLOCATOR::Allocate( length * sizeof( __TYPE__ ) );
-        
-        memcpy( (void*)pointer, ((( char* )MemoryBuffer) + Offset), (size_t) length );
-        
-        Offset  += length;
-        size = length;
+    memcpy( ( ( ( char* ) MemoryBuffer ) + Offset ), &length, sizeof( size_t ) );
+    Offset+=sizeof( size_t );
+    memcpy( ( ( ( char* ) MemoryBuffer ) + Offset ), ( void* ) pointer, length );
+    Offset+=length;
+}
+
+template < typename __TYPE__ >
+void InputBytes( __TYPE__ * & pointer, int size ) {
+
+    size_t
+        length=size * sizeof( __TYPE__ );
+
+    if ( AllocatedBytes - Offset < length + sizeof( size_t ) ) {
+
+        AllocatedBytes+=AllocatedBytes + length + sizeof( size_t );
+
+        MemoryBuffer=realloc( MemoryBuffer, AllocatedBytes );
+
+#if DEBUG
+        if ( MemoryBuffer == NULL ) {
+
+            CORE_RUNTIME_Abort();
+        }
+#endif
     }
 
-    //Stupid fix with size_t
-    template < typename __TYPE__ >
-    void OutputBytes(__TYPE__ * pointer, size_t & size ) {
-        
-        size_t
-            length;
-        
-        memcpy( &length, (( char* )MemoryBuffer + Offset), sizeof(size_t) );
-        Offset  += sizeof(size_t);
-        
-        memcpy( (void*)pointer, ((( char* )MemoryBuffer) + Offset), (size_t) length );
-        
-        Offset  += length;
-        
-        size = length;
-    }
+    memcpy( ( ( ( char* ) MemoryBuffer ) + Offset ), &length, sizeof( size_t ) );
+    Offset+=sizeof( size_t );
+    memcpy( ( ( ( char* ) MemoryBuffer ) + Offset ), ( void* ) pointer, length );
+    Offset+=length;
+}
 
-    template < typename __TYPE__ >
-    void OutputBytesWithoutSize(__TYPE__ * pointer, size_t & size ) {
-        
-        memcpy( (void*)pointer, ((( char* )MemoryBuffer) + Offset), (size_t) sizeof( __TYPE__ ) );
-        
-        Offset  += sizeof( __TYPE__ );
-        
-        size = sizeof( __TYPE__ );
-    }
+template < typename __TYPE__ >
+void OutputBytes( __TYPE__ * & pointer, int & size ) {
 
-    void * GetMemoryAtOffset() {
-        
-        return ((( char* )MemoryBuffer) + Offset);
-    }
+    size_t
+        length;
 
-private :
+    memcpy( &length, ( ( char* ) MemoryBuffer + Offset ), sizeof( size_t ) );
+    Offset+=sizeof( size_t );
+
+    pointer=( __TYPE__ * ) CORE_MEMORY_ALLOCATOR::Allocate( length * sizeof( __TYPE__ ) );
+
+    memcpy( ( void* ) pointer, ( ( ( char* ) MemoryBuffer ) + Offset ), ( size_t ) length );
+
+    Offset+=length;
+    size=length;
+}
+
+//Stupid fix with size_t
+template < typename __TYPE__ >
+void OutputBytes( __TYPE__ * pointer, size_t & size ) {
+
+    size_t
+        length;
+
+    memcpy( &length, ( ( char* ) MemoryBuffer + Offset ), sizeof( size_t ) );
+    Offset+=sizeof( size_t );
+
+    memcpy( ( void* ) pointer, ( ( ( char* ) MemoryBuffer ) + Offset ), ( size_t ) length );
+
+    Offset+=length;
+
+    size=length;
+}
+
+template < typename __TYPE__ >
+void OutputBytesWithoutSize( __TYPE__ * pointer, size_t & size ) {
+
+    memcpy( ( void* ) pointer, ( ( ( char* ) MemoryBuffer ) + Offset ), ( size_t ) sizeof( __TYPE__ ) );
+
+    Offset+=sizeof( __TYPE__ );
+
+    size=sizeof( __TYPE__ );
+}
+
+void * GetMemoryAtOffset() {
+
+    return ( ( ( char* ) MemoryBuffer ) + Offset );
+}
+
+private:
 
     void
         * MemoryBuffer;
@@ -285,6 +297,7 @@ private :
         Offset,
         Size;
 
-XS_CLASS_END
+    XS_CLASS_END
 
 #endif /* defined(__GAME_ENGINE_REBORN__CORE_DATA_STREAM__) */
+
